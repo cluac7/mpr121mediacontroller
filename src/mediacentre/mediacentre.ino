635879ada6f4356df744b167b3d9efeebdcf40ed
Rahul Saini <rahul.saini@education.nsw.gov.au>
2023-10-02 19:58:06 +1100

    removed fluff from the arduino code

--- src/mediacentre/mediacentre.ino
+++ src/mediacentre/mediacentre.ino
@@ -36,8 +36,6 @@ void setup() {
   while (!Serial) { // needed to keep leonardo/micro from starting too fast!
     delay(10);
   }
-  
-  
   // Default address is 0x5A, if tied to 3.3V its 0x5B
   // If tied to SDA its 0x5C and if SCL then 0x5D
   if (!cap.begin(0x5A)) {
@@ -55,31 +53,7 @@ void loop() {
     if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
       Serial.print(i); Serial.println("");
     }
-    // if it *was* touched and now *isnt*, alert!
-    // if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
-    //   Serial.print(i);
-    // }
   }
-
-  // reset our state
   lasttouched = currtouched;
-
-  // comment out this line for detailed data from the sensor!
   return;
-  
-  // debugging info, what
-  Serial.print("\t\t\t\t\t\t\t\t\t\t\t\t\t 0x"); Serial.println(cap.touched(), HEX);
-  Serial.print("Filt: ");
-  for (uint8_t i=0; i<12; i++) {
-    Serial.print(cap.filteredData(i)); Serial.print("\t");
-  }
-  Serial.println();
-  Serial.print("Base: ");
-  for (uint8_t i=0; i<12; i++) {
-    Serial.print(cap.baselineData(i)); Serial.print("\t");
-  }
-  Serial.println();
-  
-  // put a delay so it isn't overwhelming
-  delay(100);
 }
